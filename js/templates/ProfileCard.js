2e6b0644c2204787f5f8364d9e4a5cc97f79bb87
xforce35 <xforce35@hotmail.fr>
2022-02-28 08:21:54 +0100

    Nettoyage

--- js/templates/ProfileCard.js
+++ js/templates/ProfileCard.js
@@ -9,12 +9,12 @@ class ProfileCard {
         const ProfileCard= `
         <article class="photographer__profileCard">
             <div class="photographer__profileCard__infos">
-                    <h1 class="photographer__profileCard__infos__name">${this._index.name}</h1>
-                    <p class="photographer__profileCard__infos__location">${this._index.city}, ${this._index.country}</p>
-                    <p class="photographer__profileCard__infos__description">${this._index.tagline}</p>
+                    <h1 tabindex="0" class="photographer__profileCard__infos__name">${this._index.name}</h1>
+                    <p tabindex="0" class="photographer__profileCard__infos__location">${this._index.city}, ${this._index.country}</p>
+                    <p tabindex="0" class="photographer__profileCard__infos__description">${this._index.tagline}</p>
             </div>
             <button class="photographer__profileCard__contactButton">Contactez-moi</button>
-            <img class="photographer__profileCard__picture" src="public/assets/photographers/${this._index.portrait}" alt="Mimi Keel">
+            <img tabindex="0" class="photographer__profileCard__picture" src="public/assets/photographers/${this._index.portrait}" alt="Portrait de ${this._index.name}">
         </article>
     `
     
--- js/templates/VideoLightbox.js
+++ js/templates/VideoLightbox.js
@@ -1,7 +1,7 @@
 export default class VideoLightBox {
     constructor({title, src}) {
         const content = `            
-            <video controls width ="500" class="works-lightbox-media-video" src="public/assets/media/${src}" alt="${title}" type="video/mp4"></video>
+            <video controls class="works-lightbox-media-video" src="public/assets/media/${src}" alt="${title}" type="video/mp4"></video>
             <div class="works-lightbox-name">${title}</div>
         `
 
